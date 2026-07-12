bca0cdbef0eae76321ad17a7d44b8aed7f5de071
Timez-zx <55596336+Timez-zx@users.noreply.github.com>
2026-07-12 21:56:31 +0000

    Deploying to gh-pages from @ Timez-zx/timez-zx.github.io@fbb8dfb9f75be44d07812fde78a3f00fb9f6b20f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Timez-zx/homepage",
+    "data-repo": "Timez-zx/timez-zx.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
